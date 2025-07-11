14a81a7dfade47d34f59d13cb72e0e205a36790f
varoon1618 <114831390+varoon1618@users.noreply.github.com>
2025-07-11 18:35:42 +0100

    added win checks fix bugs

--- Game.py
+++ Game.py
@@ -1,6 +1,21 @@
 import Player
 import random
 
+'''
+TO DO:
+
+Add big blind, small blind -make sure it rotates each round
+
+Calculate different ways of winning/who won
+
+Keepingtrack of who folded, who is playing , who is totally out of table(doesnt have enough money)
+
+Add frontend 
+
+Any bugs ?
+
+'''
+
 class Game:
   players = []
   communityCards = []
@@ -18,7 +33,7 @@ class Game:
     
   def main(self):
     self.initialiseGame()
-    while(self.state<9):
+    while(self.state<11):
       self.playGame()
   
   
@@ -50,7 +65,10 @@ class Game:
       self.previousBet = 5
       self.collectBets()
       #print("total pot: ",self.pot)
-      self.state +=1
+      if(not(self.checkGameOver())):
+        self.state +=1
+      else:
+        self.state=10
     
     if(self.state==3):
       self.previousBet = 5
@@ -61,11 +79,19 @@ class Game:
     if (self.state==5 or self.state==7):
       self.previousBet = 5
       self.dealRandomCard()
+      print('community cards: ', " ".join(str(card) for card in self.communityCards))
       self.state += 1
       
     if (self.state == 9):
-      print("showdown")
+      for player in [p for p in self.players if not(p.fold)]:
+        print(player.name,'has hand: ',player.printHand())
+      
+      print('community cards: ', " ".join(str(card) for card in self.communityCards))
+      self.state +=1
     
+    if (self.state==10):
+      print("The winner is: ", self.calculateWinner())
+      self.state = 11
     
   def initialiseRound(self):
     self.cards = self.spades + self.hearts + self.clubs + self.diamonds
@@ -79,7 +105,7 @@ class Game:
     
   def collectBuyIns(self,buyIn):
     for player in self.players:
-      if(buyIn < player.money):
+      if(buyIn < player.money) and not(player.fold):
         self.pot = self.pot + player.bet(buyIn)
       else:
         print("Player "+player.name+" cannot afford buy in, kicked out")
@@ -97,7 +123,7 @@ class Game:
   
   
   def collectBets(self):
-    for player in self.players:
+    for player in [p for p in self.players if not(p.fold)]:
       if(player.name == "varun"):
         print("What is your next move ?")
         print("Previous bet: ",self.previousBet)
@@ -135,6 +161,115 @@ class Game:
     self.cards.remove(card)
     for player in self.players:
       player.setCommunityCards(self.communityCards)
+  
+  def checkGameOver(self):
+    if(len([p for p in self.players if not(p.fold)]) == 1):
+      return True
+    return False
+  
+  def calculateWinner(self):
+    remainingPlayers = [p for p in self.players if not(p.fold)]
+    if(len(remainingPlayers) ==1):
+      return remainingPlayers[0].name
+    else:
+      scores = [self.scoreHand(p) for p in remainingPlayers]
+      maximum = max(scores)
+      if(scores.count(maximum) ==1):
+        return remainingPlayers[scores.index(maximum)].name
+      else:
+        print("NEED TO IMPLEMENT")
+        return("ABC")
+  
+  def scoreHand(self,player):
+    if self.isSameSuit(player):
+      if self.isConsecutive(player):
+        if self.isRoyalFlush(player):
+          return 10 #royal flush
+        else:
+          return 9 #straight flush
+      else:
+        return 6 #flush
+    
+    if self.isSameValue(player):
+      return 8 #four of a kind
+    
+    if self.isConsecutive(player):
+      return 5 #straight
+    
+    if (self.returnTriplets(player)>0):
+      if(self.returnPairs(player)==1):
+        return 7 #full house
+      else:
+        return 4 #three of a kind
+    
+    if(self.returnPairs(player)==2):
+      return 3 # 2 pairs
+    
+    if(self.returnPairs(player)==1):
+      return 2 # 1 pair
+    
+    return 1 #nothing 
+  
+  
+  def isSameSuit(self, player):
+    cards = player.hand + self.communityCards
+    if not cards:
+      return False
+    suits = [card[0] for card in cards]
+    first_suit = suits[0]
+    return all(suit == first_suit for suit in suits)
+  
+  def isConsecutive(self,player):
+    cards =  player.hand + self.communityCards
+    values = [int(c[1:]) for c in cards]
+    values.sort()
+    for i in range(len(values) - 1):
+      if values[i + 1] != values[i] + 1:
+        return False
+    return True
+  
+  def isSameValue(self,player):
+    cards = player.hand+ self.communityCards
+    values = [int(c[1:]) for c in cards]
+    for i in range(len(values)-1):
+      if values[i+1] != values[i]:
+        return False
+      return True
+  
+  def returnPairs(self,player):
+    cards = player.hand+self.communityCards
+    values = [int(c[1:]) for c in cards]
+    counts = [values.count(v) for v in values]
+    return int(counts.count(2)/2)
+  
+  def returnTriplets(self,player):
+    cards = player.hand+self.communityCards
+    values = [int(c[1:]) for c in cards]
+    counts = [values.count(v) for v in values]
+    return int(counts.count(3)/3)
+    
+  '''
+  Royal FLush - A,K,Q,J,10 - all same suite
+  
+  Straight Flush - same suit with 5 consecutive vlaues from that suit
+  
+  Four of a kind 
+  
+  Full house - 3 of a kind + 2 of a kind
+  
+  Flush - 5 cards same suit, can be in any order 
+  
+  Straight - 5 consecutive cards of any suit
+  
+  Three of a kind 
+  
+  Two pairs
+  
+  One Pair
+  
+  High card - if none of the above then the person with highest card wins, if both
+  players have same highest card, second highest card is used 
+  '''
   
     
     
